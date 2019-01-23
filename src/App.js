f1e18e1a2e56100fb5ec7bed40eec4f6a4fb8ac3
lebrancconvas <lebrancconvas@gmail.com>
2019-01-23 07:40:26 +0700

    Use useEffect and Add style to Button.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
+
+  useEffect(() => {
+    document.title = `Clicked ${count} times.`
+  })
 
   return (
     <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <p>นายคลิกไป {count} ครั้งแล้วนะ</p>
+      <button
+        style={{
+          padding: '8px 16px',
+          borderRadius: 4,
+          fontSize: '1.25rem'
+        }} 
+        onClick={() => setCount(count + 1)}
+        >
+        Click Please!!
+        </button>
     </div>
   )
 }
