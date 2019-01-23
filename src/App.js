a3c3b2ea87de56884887f9aebb7a2342ff240298
lebrancconvas <lebrancconvas@gmail.com>
2019-01-23 20:48:45 +0700

    Complete Project.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React, { useState , useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = () => {
-  const [count, setCount] = useState(1)
+  const [name, setName] = useState(null);
 
-  useEffect(() => {
-    document.title = `Clicked ${count} times.`
-  })
+  function handleInput(e) {
+    setName(e.target.value);
+  }
 
   return (
-    <div>
-      <p>นายคลิกไป {count} ครั้งแล้วนะ</p>
-      <button
-        style={{
-          padding: '8px 16px',
-          borderRadius: 4,
-          fontSize: '1.25rem'
-        }} 
-        onClick={() => setCount(count + 1)}
-        >
-        Click Please!!
-        </button>
+    <div className="App-header">
+      <h1>Mega Engine</h1>
+      <p>Search that you wanna search.</p>
+      <p>Search Topic : {name}</p>
+      <form method="GET" action="https://google.com/search">
+        <input
+          style={{ borderRadius: 4, height: "40%" }}
+          type="search"
+          name="q"
+          onChange={handleInput}
+        />
+        <input style={{ margin: "10px" }} type="submit" value="Submit" />
+        <div>
+          <input id="search" type="radio" name="tbm" value="" />
+          <label for="search">All</label>
+        </div>
+        <div>
+          <input id="imagesearch" type="radio" name="tbm" value="isch" />
+          <label for="imagesearch">Image</label>
+        </div>
+        <div>
+          <input id="videosearch" type="radio" name="tbm" value="vid" />
+          <label for="videosearch">Video</label>
+        </div>
+        <div>
+          <input id="newssearch" type="radio" name="tbm" value="nws" />
+          <label for="newssearch">News</label>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default App;
