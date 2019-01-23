ee09876ef110a31a3a2e170314d52e55653d92b7
lebrancconvas <lebrancconvas@gmail.com>
2019-01-24 05:33:31 +0700

    Add Search Status.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import "./App.css";
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
       <h1>Mega Engine</h1>
       <p>Search that you wanna search.</p>
       <p>Search Topic : {name}</p>
+      <p id="status"></p>
       <form method="GET" action="https://google.com/search">
         <input
           style={{ borderRadius: 4, height: "40%" }}
@@ -21,21 +22,37 @@ const App = () => {
           onChange={handleInput}
         />
         <input style={{ margin: "10px" }} type="submit" value="Submit" />
-        <div>
-          <input id="search" type="radio" name="tbm" value="" />
-          <label for="search">All</label>
+        <div id="first-col">
+          <div>
+            <input id="search" type="radio" name="tbm" value="" />
+            <label for="search">All</label>
+          </div>
+          <div>
+            <input id="imagesearch" type="radio" name="tbm" value="isch" />
+            <label for="imagesearch">Image</label>
+          </div>
+          <div>
+            <input id="videosearch" type="radio" name="tbm" value="vid" />
+            <label for="videosearch">Video</label>
+          </div>
+          <div>
+            <input id="newssearch" type="radio" name="tbm" value="nws" />
+            <label for="newssearch">News</label>
+          </div>
         </div>
-        <div>
-          <input id="imagesearch" type="radio" name="tbm" value="isch" />
-          <label for="imagesearch">Image</label>
-        </div>
-        <div>
-          <input id="videosearch" type="radio" name="tbm" value="vid" />
-          <label for="videosearch">Video</label>
-        </div>
-        <div>
-          <input id="newssearch" type="radio" name="tbm" value="nws" />
-          <label for="newssearch">News</label>
+        <div id="second-col">
+          <div>
+            <input id="placesearch" type="radio" name="tbm" value="plcs" />
+            <label for="placesearch">Place</label>
+          </div>
+          <div>
+            <input id="booksearch" type="radio" name="tbm" value="bks" />
+            <label for="booksearch">Books</label>
+          </div>
+          <div>
+            <input id="shopsearch" type="radio" name="tbm" value="shop" />
+            <label for="shopsearch">Shop</label>
+          </div>
         </div>
       </form>
     </div>
